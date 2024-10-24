651248c66f2a0c4fe5f3ec62253ba113e5c5af8e
Charles Kitchen <charleskitchen@gmail.com>
2024-10-24 13:10:26 -0400

    improper formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-8">
       <div className="max-w-md mx-auto">
+        {" "}
         <h1 className="text-2xl font-bold mb-4">Forehead Game</h1>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
